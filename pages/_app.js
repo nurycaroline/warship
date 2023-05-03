1fe649fb6578010cee8e02cf83e7a15e780d4a9d
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2023-05-03 15:17:34 -0300

    add analitics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import GlobalStyle from "../styles/globals"
-
+import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
